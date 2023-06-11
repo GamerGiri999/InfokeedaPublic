91bf7ea06e0f90029ac3a4b0bfec4412fd9e157a
Remon Islam <93038033+GamerGiri999@users.noreply.github.com>
2023-06-11 10:43:03 +0600

    Create ClickAdToGetLink.js

---
+++ adlinkfly/ClickAdToGetLink.js
@@ -0,0 +1,7 @@
+$(function(){                    
+    $('.abody').click(function() {
+        var timer=setInterval(function(){document.getElementById('plswait').style.display = 'none';},1500);
+    var timer=setInterval(function(){document.getElementById('myDIV').style.display = 'block';},1500);
+    var timer=setInterval(function(){document.getElementById('secondtext').style.display='block';},1500); 
+     }); 
+});
