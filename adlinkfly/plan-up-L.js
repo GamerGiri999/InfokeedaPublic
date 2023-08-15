6753ad9cc5a0d44a91e5e138a998d9274d409b6a
Remon Islam <93038033+GamerGiri999@users.noreply.github.com>
2023-08-15 14:10:27 +0600

    Update plan-up-L.js

--- adlinkfly/plan-up-L.js
+++ adlinkfly/plan-up-L.js
@@ -1,21 +1,12 @@
 fetch('https://api.npoint.io/9cb32504c6a002f95d21')
-        .then(response => response.json())
-        .then(data => {
-         
-            const currentDomain = window.location.hostname;
-        
-            const matchedDomain = data.find(obj => currentDomain === obj.currentDomain || currentDomain.endsWith('.' + obj.currentDomain));
-            
-            if (!matchedDomain) {
-                document.getElementById('centered-content').innerHTML = '<h1>Invalid License Contact Admin For License</h1>';
-                setTimeout(function() {
-                    window.location.href = 'https://cutt.ly/licen';
-                }, 1000); // Redirect after 3 seconds (adjust this value as needed)
-            }
-            else {
-                setTimeout(function() {
-                    window.location.href = '/member/dashboard';
-                }, 3000);
-                }
-        });
-        
+  .then(response => response.json())
+  .then(data => {
+    const currentDomain = window.location.hostname;
+    const matchedDomain = data.find(obj => currentDomain === obj.currentDomain || currentDomain.endsWith('.' + obj.currentDomain));
+    if (!matchedDomain) {
+      document.body.innerHTML = '<h1>Invalid License Contact Admin For License</h1>';
+      setTimeout(function() {
+        window.location.href = 'https://cutt.ly/licen';
+      }, 3000); // Redirect after 3 seconds (adjust this value as needed)
+    }
+  });
