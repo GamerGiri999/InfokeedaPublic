689b1e51e4aecfd8ae3d4203ff410d09739d5b7f
Remon Islam <93038033+GamerGiri999@users.noreply.github.com>
2024-03-24 23:24:43 +0600

    Update cookieds.js

--- webzeni/cookieds.js
+++ webzeni/cookieds.js
@@ -30,3 +30,32 @@
     // Insert the code after the button
     button.parentNode.insertBefore(tempDiv, button.nextSibling);
   }
+
+//
+
+// Check if the cookie named "short" is present
+if (document.cookie.indexOf("short") !== -1) {
+    // Create the elements to be inserted
+    var adContainer = document.createElement('div');
+    adContainer.setAttribute('id', 'div-gpt-ad-1711300124662-0');
+    adContainer.setAttribute('style', 'min-width: 250px; min-height: 50px;');
+    
+    var adScript = document.createElement('script');
+    adScript.innerHTML = "googletag.cmd.push(function() { googletag.display('div-gpt-ad-1711300124662-0'); });";
+    
+    // Find the element with id "bottom"
+    var divBottom = document.getElementById('bottom');
+    
+    // Check if the "bottom" div exists
+    if (divBottom) {
+        // Insert the ad container and script before the div with id "bottom"
+        divBottom.parentNode.insertBefore(adContainer, divBottom);
+    } else {
+        // If the "bottom" div doesn't exist, insert at the end of the document body
+        document.body.appendChild(adContainer);
+    }
+    
+    // Append the script to the ad container
+    adContainer.appendChild(adScript);
+}
+
