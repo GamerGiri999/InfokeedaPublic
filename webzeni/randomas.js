e6092fef01806737749daaf3c22e4e008b0d6083
Remon Islam <93038033+GamerGiri999@users.noreply.github.com>
2024-03-26 11:31:03 +0600

    Update randomas.js

--- webzeni/randomas.js
+++ webzeni/randomas.js
@@ -1,18 +1,22 @@
-  var slots = [
-        googletag.defineSlot('/23033789617/bottom', [[250,250],[300,250]], 'div-5'),
-        googletag.defineSlot('/23033789617/bottom2', [[250,250],[300,250]], 'div-5')
-    ];
-
-    // Choose a random slot index
-    var randomIndex = Math.floor(Math.random() * slots.length);
-
 window.googletag = window.googletag || {cmd: []};
 googletag.cmd.push(function() {
-    // Define multiple slots
-  
+    // Define slots
+    var slot1 = googletag.defineSlot('/23033789617/bottom', [[250,250],[300,250]], 'div-5').addService(googletag.pubads());
+    var slot2 = googletag.defineSlot('/23033789617/bottom2', [[250,250],[300,250]], 'div-5').addService(googletag.pubads());
 
-    // Use the randomly selected slot
-    slots[randomIndex].addService(googletag.pubads());
+    // Choose a random index
+    var randomIndex = Math.floor(Math.random() * 2); // 2 is the number of slots
+
+    // Enable the selected slot
+    if (randomIndex === 0) {
+        slot1.setCollapseEmptyDiv(true);
+        slot1.setForceSafeFrame(true);
+        slot1.setCentering(true);
+    } else {
+        slot2.setCollapseEmptyDiv(true);
+        slot2.setForceSafeFrame(true);
+        slot2.setCentering(true);
+    }
 
     // Enable other pubads configurations
     googletag.pubads().enableSingleRequest();
