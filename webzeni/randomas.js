dd719fc9b812ce54a18fd7e92e7fa11aa390ded3
Remon Islam <93038033+GamerGiri999@users.noreply.github.com>
2024-03-26 11:27:09 +0600

    Update randomas.js

--- webzeni/randomas.js
+++ webzeni/randomas.js
@@ -1,13 +1,15 @@
-window.googletag = window.googletag || {cmd: []};
-googletag.cmd.push(function() {
-    // Define multiple slots
-    var slots = [
+  var slots = [
         googletag.defineSlot('/23033789617/bottom', [[250,250],[300,250]], 'div-5'),
         googletag.defineSlot('/23033789617/bottom2', [[250,250],[300,250]], 'div-5')
     ];
 
     // Choose a random slot index
     var randomIndex = Math.floor(Math.random() * slots.length);
+
+window.googletag = window.googletag || {cmd: []};
+googletag.cmd.push(function() {
+    // Define multiple slots
+  
 
     // Use the randomly selected slot
     slots[randomIndex].addService(googletag.pubads());
