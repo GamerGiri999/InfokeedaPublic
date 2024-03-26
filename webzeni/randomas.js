2e5afc4e28c26a6030b6b1e4200698c4e19103b6
Remon Islam <93038033+GamerGiri999@users.noreply.github.com>
2024-03-26 11:24:24 +0600

    Update randomas.js

--- webzeni/randomas.js
+++ webzeni/randomas.js
@@ -3,31 +3,21 @@ googletag.cmd.push(function() {
     // Define multiple slots
     var slots = [
         googletag.defineSlot('/23033789617/bottom', [[250,250],[300,250]], 'div-5'),
-        googletag.defineSlot('/23033789617/bottom2', [[250,250],[300,250]], 'div-5'),
-        googletag.defineSlot('/23033789617/Inarticle', [[250,250],[300,250]], 'div-5')
+        googletag.defineSlot('/23033789617/bottom2', [[250,250],[300,250]], 'div-5')
     ];
 
-    // Get a random index
+    // Choose a random slot index
     var randomIndex = Math.floor(Math.random() * slots.length);
 
     // Use the randomly selected slot
-    var selectedSlot = slots[randomIndex];
+    slots[randomIndex].addService(googletag.pubads());
 
-    // Add service and other settings
-    selectedSlot.addService(googletag.pubads());
-
-    // Append cloned containers to the DOM
-    for (var i = 0; i < slots.length; i++) {
-        var container = document.getElementById('div-5');
-        var clonedContainer = container.cloneNode(true);
-        clonedContainer.id = 'div-5-' + (i + 1); // Assigning a unique ID to each cloned container
-        container.parentNode.appendChild(clonedContainer);
-        googletag.display(clonedContainer.id);
-    }
-
+    // Enable other pubads configurations
     googletag.pubads().enableSingleRequest();
     googletag.pubads().collapseEmptyDivs();
     googletag.pubads().setForceSafeFrame(true);
     googletag.pubads().setCentering(true);
+
+    // Enable services
     googletag.enableServices();
 });
